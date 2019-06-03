c3063b4221a25feef48fb7265a46b65fec8b390e
jrbarbati <jrbarbati@icloud.com>
2019-06-03 18:05:02 -0400

    adding mining stats - total xp gained, xp/hour etc.

--- src/bots/MiningBot.java
+++ src/bots/MiningBot.java
@@ -2,33 +2,39 @@ package bots;
 
 import input.Input;
 import logger.Logger;
+import org.powerbot.script.PaintListener;
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.Script;
-import org.powerbot.script.Tile;
 import org.powerbot.script.rt4.ClientContext;
+import org.powerbot.script.rt4.Constants;
 import rock.Rocks;
-import tasks.Banking;
 import tasks.Mining;
 import tasks.Task;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Script.Manifest(
         name="Mining",
         description = "Automatically Mines Coal from Dwarven Mine",
         properties = "author=VirHircinus; topic=999; client=4;"
 )
-public class MiningBot extends PollingScript<ClientContext>
+public class MiningBot extends PollingScript<ClientContext> implements PaintListener
 {
     private List<Task> tasks = new ArrayList<Task>();
     private Logger log = new Logger(this.getClass());
     private Rocks rocks = new Rocks();
+    private Map<String, Integer> startingXp = new HashMap<>();
 
     @Override
     public void start()
     {
         log.info("Started");
+
+        startingXp.put("Mining", currentXp(Constants.SKILLS_MINING));
 
         String selectedOre = Input.popupDialogWithOptions("Which ore would you like to mine?", rocks.types());
 
@@ -67,5 +73,42 @@ public class MiningBot extends PollingScript<ClientContext>
     public void stop()
     {
         log.info("Stopped");
+    }
+
+    @Override
+    public void repaint(Graphics graphics)
+    {
+        Graphics2D g = (Graphics2D) graphics;
+
+        int miningXpGained = calculateXpGained("Mining", Constants.SKILLS_MINING);
+
+        g.setColor(new Color(0, 0, 0, 180));
+        g.fillRect(0, 0, 200, 80);
+
+        g.setColor(new Color(255, 255, 255, 180));
+        g.drawString("Mining Bot Session Stats", 20, 20);
+
+        g.drawString(String.format("Total XP Gained: %d", miningXpGained), 20, 40);
+        g.drawString(String.format("XP/Hour:         %.0f", calculateXpPerHour(miningXpGained)), 20, 60);
+    }
+
+    private int calculateXpGained(String skillKey, int skill)
+    {
+        return currentXp(skill) - startingXp.get(skillKey);
+    }
+
+    private double calculateXpPerHour(int xpGained)
+    {
+        return xpGained / runtimeInHours();
+    }
+
+    private int currentXp(int skill)
+    {
+        return ctx.skills.experience(skill);
+    }
+
+    private double runtimeInHours()
+    {
+        return getTotalRuntime() / 3600000d;
     }
 }
